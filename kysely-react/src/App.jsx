2f4dd7829597807f3623cf11118a6bc3bcf1a873
bhi116 <bhi116@myy.haaga-helia.fi>
2025-11-06 12:58:29 +0200

    AgGrid

--- kysely-react/src/App.jsx
+++ kysely-react/src/App.jsx
@@ -1,8 +1,14 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import { AgGridReact } from "ag-grid-react";
+import { useRef } from 'react';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 function App() {
   const [kyselyt, setKyselyt] = useState([]);
+  const gridRef = useRef();
 
   useEffect(() => {
     fetch('http://localhost:8080/api/kyselyt')
@@ -12,15 +18,28 @@ function App() {
     
       return response.json();
       })
-      .then(responseData => setKyselyt(responseData.data))
+      .then(responseData => setKyselyt(responseData))
       .catch(err => console.error(err))
   }, []);
+
+  const columns = [
+    {headerName: 'Id', field: 'kyselyId'},
+    {headerName: 'Nimi', field: 'nimi'},
+    {headerName: 'Kuvaus', field: 'kuvaus'}
+  ]
 
 
   return (
     <div>
       <h1>Kyselyt</h1>
-      
+        <AgGridReact
+          ref={gridRef}
+          onGridReady={ params => gridRef.current = params.api}
+          rowData={kyselyt}
+          columnDefs={columns}
+          rowSelection={{ mode: 'singleRow', checkboxes: false}}
+
+        />
     </div>
   )
 }
