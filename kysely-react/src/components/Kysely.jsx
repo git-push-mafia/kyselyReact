e83736d02c7a541c4b1cd64b476a5e873ba3a583
bhi116 <bhi116@myy.haaga-helia.fi>
2025-11-18 16:47:53 +0200

    MUI + tallennus nappi

--- kysely-react/src/components/Kysely.jsx
+++ kysely-react/src/components/Kysely.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 
 export default function Kysely() {
@@ -7,6 +8,8 @@ export default function Kysely() {
     const [kysely, setKysely] = useState(null);
 
     const {kyselyId} = useParams();
+
+    
 
     useEffect(() => {
 
@@ -25,20 +28,61 @@ export default function Kysely() {
 
     if (!kysely) return <div>Ladataan...</div>;
 
+    const handleSubmitAnswer = () => {
+
+        console.log("bling blang blong");
+
+        fetch('http://localhost:8080/api/vastaukset', {
+            method: "POST",
+            headers: { "Content-Type" : "application-json" },
+            body: JSON.stringify()
+        })
+        .then(response => {
+            if(!response.ok) {
+                throw new Error("Error in fetching data :(");
+            }
+            alert("Maybe works???");
+
+        })
+        .catch(err => console.error(err));
+    }
+
+
 
     return (
-        <div style={{ width: '100%', textAlign: 'left' }}>
-            <h1>{kysely.nimi}</h1>
+    /*   /*  <div style={{ width: '100%', textAlign: 'left' }}>
+            {/* <h1>{kysely.nimi}</h1>
             <div>{kysely.kuvaus}</div>
-            <ol>
+            <ol>}
                 {Array.isArray(kysely?.kysymykset) && kysely.kysymykset.length > 0 ? (
                 kysely.kysymykset.map((item) => (
-                    <li key={item.kysymysId}>{item.kysymys}</li>
+                    <><li key={item.kysymysId}>{item.kysymys}</li>
+                    
+                    <input
+                        key={item.vastausId}
+                        type="text"
+                        name="vastaus"
+                        placeholder="Vastaa tähän"
+                        value={kysely.vastaus} /></>
                 ))
                 ) : (
                 <li>No questions available.</li>
                 )}
-            </ol>
-        </div>
+            </ol> }
+            <Button onClick={handleSubmitAnswer}>Tallenna vastaukset</Button>
+        </div> */
+        <form onSubmit={handleSubmitAnswer}>
+            <TextField
+                placeholder="KIRJOITA VASTAUS TÄHÄN LOL"
+                value={kysely.vastaus}
+                name= "vastaus"
+                label= "Vastaus"
+            />
+
+            <Button type="submit" variant="contained" color="primary">
+                 Submit
+            </Button>
+        </form>
+
     )
 }
